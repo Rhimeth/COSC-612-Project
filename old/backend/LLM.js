4df412272271059a08f0839fb62392a100741627
LN167 <ln167.contact@gmail.com>
2024-04-15 19:14:09 -0400

    all test working

--- old/backend/LLM.js
+++ old/backend/LLM.js
@@ -2,7 +2,7 @@
 Currently setup to work with OpenAI's API
 */
 
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -23,10 +23,9 @@ export class LLM {
     }
     instance = this;
 
-    const configuration = new Configuration({
-      apiKey: apiKey,
+    const openAIClient = new OpenAI({
+      apikey: process.env["OPENAI_API_KEY"],
     });
-    this.openai = new OpenAIApi(configuration);
   }
 
   // API Call
@@ -36,32 +35,38 @@ export class LLM {
       this.promptAndResponse.set(prompt, response);
     } catch (error) {
       console.error("Query method failed", error);
-      return null;
+      throw error;
     }
   }
-
-
-
 
   // Called inside query()
   async __callLLM(prompt) {
     if (typeof prompt !== "string") {
       throw new Error("Prompt must be a string");
     }
-    if (!prompt) {
-      throw new Error("Invalid prompt");
-    }
     if (prompt.length === 0) {
       throw new Error("Prompt cannot be empty");
+    }
+    if (!prompt) {
+      throw new Error("Invalid prompt");
     }
     if (prompt.length < 11) {
       throw new Error("Prompt must be more than 10 characters");
     }
     try {
-      const response = await this.openai.createCompletion({
+      const response = await this.openAIClient.chat.completions.create({
         model: model, // model name
-        prompt: String.concat(prompt, "Only answer in two sentences."),
-        max_tokens: 150,
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a culinary expert but you express it overtly because you are a chatbot of few words. When users ask you about anything other than food, recipes, cooking, ingredients, or descriptions of recipes, you should not respond. You should respond with only one good sentence, followed by 5 possible food titles that either 1) have the ingredients they are interested in used in the food, or 2) may be a food or recipe the user is describing but doesn't know the name of. For example the user may say 'I was at a BBQ and there was this food on a stick, some were peppers, but had other vegetables that idk what they were', you may give you best 5 guess for what it could have been.",
+          },
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
       });
       return response.data.choices[0].text.trim();
     } catch (error) {
@@ -69,6 +74,6 @@ export class LLM {
       throw error;
     }
   }
-
-
 }
+
+export default LLM;
